9467c844d7ed5355a2d933d2ef459681f69e71d2
AlvinLiem <avh8project2023@gmail.com>
2023-12-20 04:24:33 +0700

    feat: logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Navbar() {
+  const navigate = useNavigate();
+  function handleLogout(e) {
+    e.preventDefault();
+    localStorage.clear();
+    navigate(`/login`);
+  }
   return (
     <>
       <nav className="bg-white border-gray-200 py-2.5 ">
@@ -12,7 +20,8 @@ export default function Navbar() {
           <div className="flex items-center lg:order-2">
             <div className="hidden mt-2 mr-4 sm:inline-block"></div>
             <button
-              type="submit"
+              onClick={handleLogout}
+              type="button"
               className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
             >
               Log Out
