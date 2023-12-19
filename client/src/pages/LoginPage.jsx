90bbd9f61e4376d4a259e8b279e637f5dda04955
AlvinLiem <avh8project2023@gmail.com>
2023-12-19 22:01:41 +0700

    feat: google-login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,34 +1,54 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, provider } from "../firebaseConfig";
+import { signInWithPopup } from "firebase/auth";
 
 const LoginPage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const { user } = await signInWithPopup(auth, provider);
+      localStorage.access_token = user.accessToken;
+      localStorage.name = user.displayName;
+      localStorage.email = user.email;
+      localStorage.photo = user.photoURL;
+      navigate(`/`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="bg-gradient-to-r from-amber-500 to-pink-500">
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="min-h-1/2 bg-gray-900 border border-gray-900 rounded-2xl bg-gradient-to-r from-slate-900 to-slate-700">
-        <div className="mx-4 sm:mx-24 md:mx-34 lg:mx-56 flex items-center space-y-4 py-12 font-semibold text-gray-500 flex-col text-center">
-          <img
-            className="h-12 w-12"
-            src="https://www.svgrepo.com/show/355037/google.svg"
-            alt="Google Logo"
-          />
-          <h1 className="text-white text-2xl">Hello! Login to access your chat!</h1>
-          <button type="submit" className="flex items-center bg-white dark:bg-gray-900 border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 dark:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 mx-auto mt-2" onClick={() => {
-            navigate('/')
-          }}>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="min-h-1/2 bg-gray-900 border border-gray-900 rounded-2xl bg-gradient-to-r from-slate-900 to-slate-700">
+          <div className="mx-4 sm:mx-24 md:mx-34 lg:mx-56 flex items-center space-y-4 py-12 font-semibold text-gray-500 flex-col text-center">
             <img
-              className="h-6 w-6 mr-2"
+              className="h-12 w-12"
               src="https://www.svgrepo.com/show/355037/google.svg"
               alt="Google Logo"
             />
-            Continue with Google
-          </button>
+            <h1 className="text-white text-2xl">
+              Hello! Login to access your chat!
+            </h1>
+            <button
+              type="submit"
+              className="flex items-center bg-white dark:bg-gray-900 border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 dark:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 mx-auto mt-2"
+              onClick={handleLogin}
+            >
+              <img
+                className="h-6 w-6 mr-2"
+                src="https://www.svgrepo.com/show/355037/google.svg"
+                alt="Google Logo"
+              />
+              Continue with Google
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
